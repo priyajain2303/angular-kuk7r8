43cdd66fe033b6e997709192c5f66a53d8bb53ca
priyajain2303 <priya.gs.cs@gmail.com>
2020-04-23 12:10:32 +0530

    Adding purchase form

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {CartService} from '../cart.service';
 
+import { FormBuilder } from '@angular/forms';
+
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
@@ -9,11 +11,24 @@ import {CartService} from '../cart.service';
 export class CartComponent implements OnInit {
 
   items;
+  checkoutForm;
 
-  constructor(private cartService: CartService) { }
+  constructor(private cartService: CartService, 
+              private formBuilder: FormBuilder,) { }
 
   ngOnInit() {
     this.items = this.cartService.getItems();
+    this.checkoutForm = this.formBuilder.group({
+      name: '',
+      address: ''
+    });
+  }
+
+  onSubmit(customerData) {
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
+
+    console.warn('Your order has been submitted', customerData);
   }
 
 
